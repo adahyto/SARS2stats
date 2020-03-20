4f8f81c048579c196cbc80859fec6d91c5a17e82
adahyto <adahy.to@gmail.com>
2020-03-20 16:35:53 +0100

    favicon

---
+++ server.js
@@ -0,0 +1,19 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/CORONA'));
+
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname+'/dist/CORONA/ngsw-worker.js'));
+});
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/CORONA/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
